92836e55166014d06805a272731b8c56a6c2af1d
Jaya0705 <47595535+Jaya0705@users.noreply.github.com>
2020-03-26 15:06:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import {Nav} from 'react-bootstrap';
+// import './css/Linkstyle.css';
+
+// function App() {
+//   return (
+//       <div>
+//           <Nav className="justify-content-end" activeKey="/home" style={{backgroundColor: "black"}}>
+//               <Nav.Item>
+//                   <Nav.Link href="/home">Home</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                   <Nav.Link eventKey="link-1">Link</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                  <Nav.Link eventKey="link-2">Link</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="disabled" disabled>
+//                   DEV DASHBOARD
+//                 </Nav.Link>
+//               </Nav.Item>
+//           </Nav>
+//       </div>    
+//   );
+// }
+
+// export default App;
